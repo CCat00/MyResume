1fc9287accfc787dae16966e675f24430034a2f3
韩威 <hw1627@163.com>
2016-07-18 14:20:20 +0800

    Initial Commit

---
+++ MyResume/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  MyResume
+//
+//  Created by 韩威 on 16/7/18.
+//  Copyright © 2016年 韩威. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ MyResume/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  MyResume
+//
+//  Created by 韩威 on 16/7/18.
+//  Copyright © 2016年 韩威. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
